6db46d3ca07a4e0559c0346c3767628d9a6a8b69
Sebastian Volz <svolz67@bg-slurmb-login1.novalocal>
2022-05-02 17:12:25 +0200

    change review error file name

--- review.sh
+++ review.sh
@@ -6,8 +6,8 @@
 #SBATCH --partition=gpu-2080ti                     # Partition to submit to
 #SBATCH --time=0-00:30           					# Runtime in D-HH:MM
 #SBATCH --mem=3G                                 # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=jthuemmel54_%j.out                  # File to which STDOUT will be written
-#SBATCH --error=jthuemmel54_%j.err                   # File to which STDERR will be written
+#SBATCH --output=svolz.out                  # File to which STDOUT will be written
+#SBATCH --error=svolz.err                   # File to which STDERR will be written
 #SBATCH --mail-type FAIL           	                # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=sebastien.volz@student.uni-tuebingen.de	# Email to which notifications will be sent
 #SBATCH --gres=gpu:1
