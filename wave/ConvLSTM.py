3af635bf3c6bd7c6ecdbed300657ba5db1af9236
baz2z <sebastian@beechey-volz.de>
2022-05-08 11:33:06 +0200

    .

--- wave/ConvLSTM.py
+++ wave/ConvLSTM.py
@@ -94,7 +94,7 @@ def visualize_wave(data, row, nbrImages = 10, fromStart = True, ):
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    batch_size = 6
+    batch_size = 60
     hidden_size = 5
     epochs = 4
     dataloader = DataLoader(dataset=Wave("wave1000-40"), batch_size=batch_size, shuffle=True, drop_last=True,
