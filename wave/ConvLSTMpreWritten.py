eb3b9bc2073d496a66079818569c8442a00e585c
baz2z <sebastian@beechey-volz.de>
2022-05-09 13:51:28 +0200

    .

--- wave/ConvLSTMpreWritten.py
+++ wave/ConvLSTMpreWritten.py
@@ -4,13 +4,16 @@ from torch import cat, tanh, Tensor, sigmoid
 
 class LSTM_cell(nn.Module):
 
-    def __init__(self, x_channels, h_channels):
+    def __init__(self, x_channels, h_channels, lateral_channels):
         super().__init__() # same?
-        self.conv = nn.Conv2d(x_channels, 4 * h_channels, 1, bias=True, padding="same")
+        self.conv = nn.Conv2d(lateral_channels, 4 * h_channels, 1, bias=True, padding="same")
+        # wenn h auch curch 1x1: hier noch h in input einfügen
+        self.transition = nn.Conv2d(x_channels + h_channels, lateral_channels, 3 ,bias=True, padding="same")
 
     def forward(self, x, h, c):
         z = torch.cat((x, h), dim=1) if x is not None else h
-        i, f, o, g = self.conv(z).chunk(chunks = 4, axis = 1)
+        l = self.transition(z)
+        i, f, o, g = self.conv(l).chunk(chunks = 4, axis = 1)
         c = sigmoid(f) * c + sigmoid(i) + tanh(g)
         h = sigmoid(o) + tanh(c)
         return h, c
@@ -23,16 +26,18 @@ class Sequence(nn.Module):
         self.in_channels = in_channels
         self.h_channels = h_channels
         self.lateral_channels = lateral_channels
-        self.transition = nn.Conv2d(self.in_channels + self.h_channels, self.lateral_channels, 3, padding="same")
-        self.lstm1 = LSTM_cell(self.lateral_channels, self.h_channels)
+        self.transition = nn.Conv2d(self.in_channels, self.lateral_channels, 3, padding="same")
+        self.lstm1 = LSTM_cell(self.lateral_channels, self.h_channels, self.lateral_channels)
         self.post = nn.Conv2d(self.h_channels, 1, 3, padding="same")
 
     def forward(self, x, steps=1):
         out = []
         h, c = torch.zeros(x.size(0), self.h_channels, 32, 32), torch.zeros(x.size(0), self.h_channels, 32, 32)
         for t in range(steps):
-            input_transition = torch.cat((x, h), dim = 1)
-            l = self.transition(input_transition)
+            # x und h in transition weil falls davor wissen was x ist wird h nicht verwendet (vllt als vgl noch in arbeit rein)
+            # v2: x und h in lstm concat
+            l = self.transition(x)
             h, c = self.lstm1(l, h, c)
+            #v2: h, c = self.lstm1(x, h, c)
             out.append(self.post(h))
         return cat(out, dim =  1)
