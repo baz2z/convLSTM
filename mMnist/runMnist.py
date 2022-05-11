c9c283e57b7c8aef187b01eae57718cb4d69b485
baz2z <sebastian@beechey-volz.de>
2022-05-11 17:19:51 +0200

    parameter tuning mnist model

--- mMnist/runMnist.py
+++ mMnist/runMnist.py
@@ -34,12 +34,12 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     batch_size = 32
     hidden_size = 5
-    epochs = 150
+    epochs = 50
     dataloader = DataLoader(dataset=mMnist(), batch_size=batch_size, shuffle=True, drop_last=True,
                             collate_fn = lambda x: default_collate(x).to(device,torch.float))
     seq = Sequence(1, hidden_size).to(device)
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(seq.parameters(), lr=0.005)
+    optimizer = optim.Adam(seq.parameters(), lr=0.0008)
     # begin to train
     loss_plot = []
     for j in range(epochs):
