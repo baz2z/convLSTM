f7e4b17b41813610094d0a24c4897fd18096399b
baz2z <sebastian@beechey-volz.de>
2022-05-18 23:18:49 +0200

    .

--- waveModels/review.sh
+++ waveModels/review.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1                                   # Ensure that all cores are on one machine
 #SBeTCH --cpus-per-task=1
 #SBATCH --partition=gpu-2080ti                     # Partition to submit to
-#SBATCH --time=0-00:30           					# Runtime in D-HH:MM
+#SBATCH --time=0-08:30           					# Runtime in D-HH:MM
 #SBATCH --mem=3G                                 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=svolz.out                  # File to which STDOUT will be written
 #SBATCH --error=svolz.err                   # File to which STDERR will be written
