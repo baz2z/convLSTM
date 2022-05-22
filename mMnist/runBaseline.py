66714bba1520353f4db32fe20292220562e3f512
baz2z <sebastian@beechey-volz.de>
2022-05-22 12:27:10 +0200

    .

--- mMnist/runBaseline.py
+++ mMnist/runBaseline.py
@@ -93,14 +93,14 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--run_idx', type=int, default=3)
+    parser.add_argument('--run_idx', type=int, default=4)
     args = parser.parse_args()
     run = args.run_idx
-    hiddenSize = 12
+    hiddenSize = 36
     seq, modelName = Forecaster(hiddenSize, baseline, num_blocks=2, lstm_kwargs={'k': 3}).to(device), "baseline"
     params = count_params(seq)
     batch_size = 32
-    epochs = 200
+    epochs = 60
     learningRate = 0.0001
     dataloader = DataLoader(dataset=mMnist("mnist-5000-60"), batch_size=batch_size, shuffle=True, drop_last=True,
                             collate_fn=lambda x: default_collate(x).to(device, torch.float))
