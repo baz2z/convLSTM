6ae5555a106e0a1c2c06a6655b1f554eee0a1fc9
baz2z <sebastian@beechey-volz.de>
2022-06-03 17:26:58 +0200

    ready for new clip test with lateral model that definitely hast spikes

--- master/runMaster.py
+++ master/runMaster.py
@@ -217,7 +217,7 @@ if __name__ == '__main__':
             loss_plot_val.append(loss)
 
     # # save model and test and train loss and parameters in txt file and python file with class
-    path = f'../trainedModels/{dataset}/{mode}/{model}-clip/{clip}/run{run}'
+    path = f'../trainedModels/{dataset}/{mode}/{model}-batch/{batch_size}/run{run}'
     if not os.path.exists(path):
         os.makedirs(path)
     os.chdir(path)
