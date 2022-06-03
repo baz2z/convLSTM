2d2d0a46baf2a09f519ede611220d7b14ff37ba5
baz2z <sebastian@beechey-volz.de>
2022-06-03 17:21:47 +0200

    ready for new clip test with lateral model that definitely hast spikes

--- master/review.sh
+++ master/review.sh
@@ -26,4 +26,4 @@ echo "RUN Script"
 
 python ./runMaster.py --run_idx ${SLURM_ARRAY_TASK_ID} --model "lateral" --dataset "wave" --datasetTrain "wave-5000-90" \
                    --datasetVal "wave-5000-90" --mode "testSpike" --context 20 --horizon 40 --learningRate 0.001 \
-                   --epochs 300 --batchSize 34 --clip 1
+                   --epochs 300 --batchSize 34 --clip 0.01
