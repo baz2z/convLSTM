bf1834960a7567384771ef5f272efe4ed1c219f8
baz2z <sebastian@beechey-volz.de>
2022-06-03 18:52:17 +0200

    run all models fpr comparison

--- master/review.sh
+++ master/review.sh
@@ -24,6 +24,6 @@ hostname
 
 echo "RUN Script"
 
-python ./runMaster.py --run_idx ${SLURM_ARRAY_TASK_ID} --model "twoLayer" --dataset "wave" --datasetTrain "wave-10000-90" \
+python ./runMaster.py --run_idx ${SLURM_ARRAY_TASK_ID} --model "skip" --dataset "wave" --datasetTrain "wave-10000-90" \
                    --datasetVal "wave-10000-90" --mode "horizon-20-40" --context 20 --horizon 40 --learningRate 0.001 \
                    --epochs 500 --batchSize 32 --clip 10
