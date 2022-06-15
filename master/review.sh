a4933fc67f6bf76dd117aa5c105c831e6eb8aff5
Sebastian Volz <svolz67@bg-slurmb-login1.novalocal>
2022-06-15 15:11:34 +0200

    .

--- master/review.sh
+++ master/review.sh
@@ -24,6 +24,6 @@ hostname
 
 echo "RUN Script"
 
-python ./runAll.py --run_idx ${SLURM_ARRAY_TASK_ID} --model "skip" --dataset "wave" --datasetTrain "wave-10000-90" \
-                   --datasetVal "wave-10000-90" --mode "horizon-20-40" --context 20 --horizon 40 --learningRate 0.001 \
-                   --epochs 400 --batchSize 32 --clip 1 --multiplier 0.5
+python ./runAll.py --run_idx ${SLURM_ARRAY_TASK_ID} --model "depthWise" --dataset "wave" --datasetTrain "wave-10000-90" \
+                   --datasetVal "wave-10000-90" --mode "horizon-20-70" --context 20 --horizon 70 --learningRate 0.001 \
+                   --epochs 400 --batchSize 32 --clip 1 --multiplier 4
