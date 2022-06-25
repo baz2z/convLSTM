9a4058ce221012b7085622e7bf58af99faec8b17
baz2z <sebastian@beechey-volz.de>
2022-06-25 17:25:46 +0200

    create horizon loss dataframe

--- plots/plotFromDataframe.py
+++ plots/plotFromDataframe.py
@@ -13,132 +13,7 @@ import matplotlib.lines as mlines
 import pandas as pd
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-def mapParas(modelName, multiplier, paramsIndex):
-    modelParams = (0, 0)
 
-    if modelName == "baseline":
-        if multiplier == 1:
-            match paramsIndex:
-                case 1:
-                    modelParams = (4, 1)
-                case 2:
-                    modelParams = (10, 1)
-                case 3:
-                    modelParams = (14, 1)
-    elif modelName == "lateral":
-        if multiplier == 0.5:
-            match paramsIndex:
-                case 1:
-                    modelParams = (10, 5)
-                case 2:
-                    modelParams = (24, 12)
-                case 3:
-                    modelParams = (36, 18)
-        if multiplier == 1:
-            match paramsIndex:
-                case 1:
-                    modelParams = (8, 8)
-                case 2:
-                    modelParams = (18, 18)
-                case 3:
-                    modelParams = (25, 25)
-        if multiplier == 2:
-            match paramsIndex:
-                case 1:
-                    modelParams = (6, 12)
-                case 2:
-                    modelParams = (13, 26)
-                case 3:
-                    modelParams = (18, 36)
-    elif modelName == "twoLayer":
-        if multiplier == 0.5:
-            match paramsIndex:
-                case 1:
-                    modelParams = (10, 5)
-                case 2:
-                    modelParams = (22, 11)
-                case 3:
-                    modelParams = (32, 16)
-        if multiplier == 1:
-            match paramsIndex:
-                case 1:
-                    modelParams = (6, 6)
-                case 2:
-                    modelParams = (15, 15)
-                case 3:
-                    modelParams = (21, 21)
-        if multiplier == 2:
-            match paramsIndex:
-                case 1:
-                    modelParams = (4, 8)
-                case 2:
-                    modelParams = (9, 18)
-                case 3:
-                    modelParams = (13, 26)
-    elif modelName == "skip":
-        if multiplier == 0.5:
-            match paramsIndex:
-                case 1:
-                    modelParams = (10, 5)
-                case 2:
-                    modelParams = (22, 11)
-                case 3:
-                    modelParams = (30, 15)
-        if multiplier == 1:
-            match paramsIndex:
-                case 1:
-                    modelParams = (7, 7)
-                case 2:
-                    modelParams = (16, 16)
-                case 3:
-                    modelParams = (23, 23)
-        if multiplier == 2:
-            match paramsIndex:
-                case 1:
-                    modelParams = (5, 10)
-                case 2:
-                    modelParams = (12, 24)
-                case 3:
-                    modelParams = (17, 34)
-    elif modelName == "depthWise":
-        if multiplier == 1:
-            match paramsIndex:
-                case 1:
-                    modelParams = (12, 1)
-                case 2:
-                    modelParams = (28, 1)
-                case 3:
-                    modelParams = (41, 1)
-        if multiplier == 2:
-            match paramsIndex:
-                case 1:
-                    modelParams = (8, 2)
-                case 2:
-                    modelParams = (20, 2)
-                case 3:
-                    modelParams = (29, 2)
-        if multiplier == 4:
-            match paramsIndex:
-                case 1:
-                    modelParams = (5, 4)
-                case 2:
-                    modelParams = (14, 4)
-                case 3:
-                    modelParams = (20, 4)
-
-    return modelParams
-def mapModel(model, hiddenSize, lateralSize):
-    match model:
-        case "baseline":
-            return Forecaster(hiddenSize, baseline, num_blocks=2, lstm_kwargs={'k': 3}).to(device)
-        case "lateral":
-            return Forecaster(hiddenSize, lateral, num_blocks=2, lstm_kwargs={'lateral_channels': lateralSize}).to(device)
-        case "twoLayer":
-            return Forecaster(hiddenSize, twoLayer, num_blocks=2, lstm_kwargs={'lateral_channels': lateralSize}).to(device)
-        case "skip":
-            return Forecaster(hiddenSize, skipConnection, num_blocks=2, lstm_kwargs={'lateral_channels': lateralSize}).to(device)
-        case "depthWise":
-            return Forecaster(hiddenSize, depthWise, num_blocks=2, lstm_kwargs={'lateral_channels_multipl': lateralSize}).to(device)
 class Wave(Dataset):
     def __init__(self, file, isTrain=True):
         # data loading
@@ -170,20 +45,6 @@ def f(x):
         2: 4
     }[x]
 
-def count_params(net):
-    '''
-    A utility function that counts the total number of trainable parameters in a network.
-    '''
-    return sum(p.numel() for p in net.parameters() if p.requires_grad)
-
-
-
-dataset = "wave"
-mode = "horizon-20-70"
-context = 20
-horizon = 170
-
-
 def matchColor(modelName):
     return{
         "baseline": "blue",
@@ -203,10 +64,15 @@ def matchMarker(multiplier):
 
 
 
+dataset = "wave"
+mode = "horizon-20-40"
+context = 20
+horizon = 70
+
 
 fig, ax = plt.subplots()
 
-df = pd.read_csv("df_70")
+df = pd.read_csv("df_40_correctStandard")
 df.reset_index()
 for index, row in df.iterrows():
     modelName = row["name"]
@@ -246,7 +112,7 @@ plt.legend(handles=[blue_line, red_line, green_line, purple_line
                     , chocolate_line, mult1, mult2, mult3, mult4], bbox_to_anchor=(1.05, 1), loc = 2)
 #plt.ylim([0.0001, 0.001])
 print()
-name = f'./createdPlots/lossToParas-{mode}-{horizon}-all'
+name = f'./createdPlots/lossToParas-{mode}-{horizon}-all-correctStand'
 fig.savefig(name, bbox_inches="tight")
 
 
