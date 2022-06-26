cba79d9c2a60d6fcdff1519161b5170281e4730e
baz2z <sebastian@beechey-volz.de>
2022-06-26 19:41:38 +0200

    get speed loss

--- speed/testNormalizeRangeSpeed.py
+++ speed/testNormalizeRangeSpeed.py
@@ -174,7 +174,7 @@ def mapDataset(speed):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default="twoLayer",
+parser.add_argument('--model', type=str, default="skip",
                     choices=["baseline", "lateral", "twoLayer", "skip", "depthWise"])
 parser.add_argument('--mode', type=str, default="horizon-20-40")
 
