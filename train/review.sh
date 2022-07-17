a2e0bc93d395465fd8e63b7b923606448d4b17d8
baz2z <sebastian@beechey-volz.de>
2022-07-17 20:05:52 +0200

    missing LR

--- train/review.sh
+++ train/review.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --array=1                             # Number of tasks (see below)
+#SBATCH --array=3                             # Number of tasks (see below)
 #SBATCH --nodes=1                                   # Ensure that all cores are on one machine
 #SBeTCH --cpus-per-task=1
 #SBATCH --partition=gpu-2080ti                     # Partition to submit to
