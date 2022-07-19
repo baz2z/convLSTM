c7eedf21059f158114308ad146f71365ee6b0915
baz2z <sebastian@beechey-volz.de>
2022-07-19 19:38:48 +0200

    missing LR

--- plot/plotAdaptedLoop.py
+++ plot/plotAdaptedLoop.py
@@ -28,7 +28,8 @@ def matchMarker(multiplier):
         "0_170":"s",
         "0_270":"d",
         "100_40":"+",
-        "100_170":"o"
+        "100_170":"o",
+        "100_270": "|"
     }[multiplier]
 
 fig, ax = plt.subplots()
@@ -43,12 +44,14 @@ for index, row in df.iterrows():
     loss0_270 = row["loss0_270"]
     loss100_40 = row["loss100_40"]
     loss100_170 = row["loss100_170"]
+    loss100_270 = row["loss100_270"]
     col = matchColor(modelName)
     #ax.scatter(adapted, loss0_40, color=col, s=16, alpha=0.7, marker=matchMarker("0_40"))
     #ax.scatter(adapted, loss0_170, color=col, s=16, alpha=0.7, marker=matchMarker("0_170"))
     ax.scatter(adapted, loss0_270, color=col, s=16, alpha=0.7, marker=matchMarker("0_270"))
     #ax.scatter(adapted, loss100_40, color=col, s=16, alpha=0.7, marker=matchMarker("100_40"))
     #ax.scatter(adapted, loss100_170, color=col, s=16, alpha=0.7, marker=matchMarker("100_170"))
+    ax.scatter(adapted, loss100_270, color=col, s=16, alpha=0.7, marker=matchMarker("100_270"))
 
 
 ax.set_yscale('log')
@@ -68,16 +71,18 @@ marker1 = mlines.Line2D([], [], color='gray', marker='^',
                           markersize=12, label='start: 0, horizon: 40', linestyle="none")
 marker2 = mlines.Line2D([], [], color='gray', marker='s',
                           markersize=12, label='start: 0, horizon: 170', linestyle="none")
-marker5 = mlines.Line2D([], [], color='gray', marker='s',
+marker5 = mlines.Line2D([], [], color='gray', marker='d',
                           markersize=12, label='start: 0, horizon: 270', linestyle="none")
 marker3 = mlines.Line2D([], [], color='gray', marker='+',
                           markersize=12, label='start: 100, horizon: 40', linestyle="none")
 marker4 = mlines.Line2D([], [], color='gray', marker='o',
                           markersize=12, label='start: 100, horizon: 170', linestyle="none")
+marker6 = mlines.Line2D([], [], color='gray', marker='|',
+                          markersize=12, label='start: 100, horizon: 270', linestyle="none")
 
 plt.xticks([100, 0.0], ["True", "False"])
-plt.legend(handles=[blue_line, red_line, green_line, purple_line, chocolate_line, marker1, marker2, marker3, marker4, marker5], bbox_to_anchor=(1.05, 1), loc = 2)
-#plt.ylim([0.00001, 0.01])
+plt.legend(handles=[blue_line, red_line, green_line, purple_line, chocolate_line, marker1, marker2, marker5, marker3, marker4, marker6], bbox_to_anchor=(1.05, 1), loc = 2)
+plt.ylim([0.00001, 0.01])
 print()
 name = f'./createdPlots/adaptedLoss'
 plt.xlabel("adapted training loop")
