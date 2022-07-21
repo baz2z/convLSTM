cdfd9ab57cb7ef54a0b9c30e3c769de43dfb698e
baz2z <sebastian@beechey-volz.de>
2022-07-21 20:46:13 +0200

    train speed lateral adapted

--- speed/review.sh
+++ speed/review.sh
@@ -24,6 +24,6 @@ hostname
 
 echo "RUN Script"
 
-python ./trainBorder.py --run_idx ${SLURM_ARRAY_TASK_ID} --model "baseline" --dataset "wave" \
+python ./trainBorder.py --run_idx ${SLURM_ARRAY_TASK_ID} --model "lateral" --dataset "wave" \
                    --mode "border" --context 20 --horizon 40 --learningRate 0.001 \
                    --epochs 400 --batchSize 32 --clip 1 --multiplier 1 --paramLevel 2 --start 100
