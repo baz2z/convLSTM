31e7c15e4b8917495500ec4bf6cd254b7d47d9a8
baz2z <sebastian@beechey-volz.de>
2022-07-23 16:48:23 +0200

    missing depthWise

--- train/trainAll.py
+++ train/trainAll.py
@@ -234,12 +234,12 @@ if __name__ == '__main__':
     if model == "baseline":
         mps = [1]
     elif model == "depthWise":
-        mps = [1, 2, 4]
+        mps = [2]
     else:
         mps = [0.5, 1, 2]
 
     for mp in mps:
-        for paramLevel in [1, 2, 3]:
+        for paramLevel in [2]:
             hiddenSize, lateralSize = mapParas(model, mp, paramLevel)
             seq = mapModel(model, hiddenSize, lateralSize)
             params = count_params(seq)
